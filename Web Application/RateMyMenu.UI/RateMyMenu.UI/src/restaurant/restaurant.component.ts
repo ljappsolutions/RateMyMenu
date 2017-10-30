edcb96eddef6da76b06c22adf60639ba93f38aad
jdsanchez261 <jdsanchez@ljappsolutions.com>
2017-10-29 20:42:50 -0600

    Adding routing for 2 new pages. Setting up welcome screen Setting up my favorite meals screen Adding bootstrap

--- Web Application/RateMyMenu.UI/RateMyMenu.UI/src/restaurant/restaurant.component.ts
+++ Web Application/RateMyMenu.UI/RateMyMenu.UI/src/restaurant/restaurant.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: 'restaurant',
+    templateUrl: './restaurant.component.html',
 })
 export class Restaurant {
+  public textToShow: string;
+
+  public constructor() {
+    this.textToShow = "Hello again";
+  }
+
+  public changeText() {
+    this.textToShow = "Hello 2";
+  }
 }
